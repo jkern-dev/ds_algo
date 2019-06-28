3c3b963ed4265fae75fbabed113bf78a3b5d75ca
Josh Kern <jkern.dev@gmail.com>
2019-06-27 17:03:07 -0700

    file finder

--- recursion/recursion_project/lib/problems.js
+++ recursion/recursion_project/lib/problems.js
@@ -165,7 +165,12 @@ function flatten(data) {
 // fileFinder(desktop, 'everlong.flac');            // => true
 // fileFinder(desktop, 'sequoia.jpeg');             // => false
 function fileFinder(directories, targetFile) {
-
+  for (let key in directories) {
+    if (key === targetFile || fileFinder(directories[key], targetFile)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 
