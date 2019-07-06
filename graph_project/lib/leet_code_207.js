243d41e98babb7660fd7b543db67c5cbd4485745
Josh Kern <jkern.dev@gmail.com>
2019-07-05 17:59:37 -0700

    graph project leet code

--- graph_project/lib/leet_code_207.js
+++ graph_project/lib/leet_code_207.js
@@ -1,6 +1,42 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/course-schedule/
 
-function canFinish(numCourses, prerequisites) {
 
+function buildGraph(list) {
+  let graph = {};
+  list.forEach((prereq) => {
+    let [course, pre] = prereq.map(String);
+    if (course in graph) {
+      graph[course].push(pre);
+    } else {
+      graph[course] = [pre];
+    }
+    if (!(pre in graph)) {
+      graph[pre] = [];
+    }
+  });
+  return graph;
 }
+
+function canFinish(numCourses, prerequisites) {
+  let prereq = buildGraph(prerequisites);
+  let totalCourses = Object.keys(prereq).length;
+  let completed = new Set();
+
+  let eligibleCourse = true;
+
+  while (eligibleCourse) {
+    eligibleCourse = false;
+    for (let course in prereq) {
+      let metReq = prereq[course].every((pre) => completed.has(pre));
+      if (!completed.includes(course) && metReq) {
+        completed.add(course);
+        eligibleCourse = true;
+      }
+    }
+  }
+  return completed.length === totalCourses;
+}
+
+
+canFinish(2, [[1,0]]);
--- graph_project/lib/num_regions.js
+++ graph_project/lib/num_regions.js
@@ -1,5 +1,17 @@
 function numRegions(graph) {
+  let visited = new Set();
+  let regions = 0;
+  for (let node in graph) {
+    if (depthFirst(node, graph, visited)) regions++;
+  }
+  return regions;
+}
 
+function depthFirst(node, graph, visited) {
+  if (visited.has(node)) return false;
+  visited.add(node);
+  graph[node].forEach(neighbor => depthFirst(neighbor, graph, visited));
+  return true;
 }
 
 module.exports = {
