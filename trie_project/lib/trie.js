9dceecf73a08cf04bd08b383115f35df8500972d
Josh Kern <jkern.dev@gmail.com>
2019-07-10 17:52:39 -0700

    tries project

---
+++ trie_project/lib/trie.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor() {
+    this.children = {};
+    this.isTerminal = false;
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new Node();
+  }
+}
+
+module.exports = {
+    Node,
+    Trie
+};
