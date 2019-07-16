beb97c6a03016c04f24aad99c76e506e8b269574
Josh Kern <jkern.dev@gmail.com>
2019-07-16 13:05:05 -0700

    tries insert Recur

--- trie_project/lib/trie.js
+++ trie_project/lib/trie.js
@@ -9,6 +9,19 @@ class Trie {
   constructor() {
     this.root = new Node();
   }
+
+  insertRecur(word, root=this.root) {
+    let letter = word[0];
+    if (!(letter in root.children)) {
+      root.children[letter] = new Node();
+    }
+
+    if (word.length === 1) {
+      root.children[letter].isTerminal = true;
+    } else {
+      this.insertRecur(word.slice(1), root.children[letter]);
+    }
+  }
 }
 
 module.exports = {
