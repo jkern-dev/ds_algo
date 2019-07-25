c22664e9340a94a3b399cae8f4302f34a0e50760
Josh Kern <jkern.dev@gmail.com>
2019-07-25 12:09:05 -0700

    update randomize string

--- randomize_string.js
+++ randomize_string.js
@@ -3,10 +3,14 @@ function randomize(str) {
   let res = ""
   while (arr.length) {
     let newChar = arr.splice(Math.floor(Math.random() * (arr.length - 1)), 1);
-    console.log(newChar);
     res += newChar
   }
-  return res;
+  console.log(str);
+  if (res === str) {
+    randomize(str);
+  } else {
+    return res;
+  }
 }
 
-console.log(randomize("food"));
+console.log(randomize("foodie"));
