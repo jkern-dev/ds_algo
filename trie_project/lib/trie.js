b6de970524c19cca17b9ff5d5ac27752675b0155
Josh Kern <jkern.dev@gmail.com>
2019-08-12 20:13:16 -0700

    tries

--- trie_project/lib/trie.js
+++ trie_project/lib/trie.js
@@ -23,16 +23,72 @@ class Trie {
     }
   }
 
-  // insertIter(word) {
-  //   let letter = word[0];
-  //   while (letter) {
-  //     if (!(letter in this.root.children)) {
-  //       this.root.children[letter] = new Node();
-  //     }
+  insertIter(word) {
+    let node = this.root;
+    for (let i = 0; i < word.length; i++) {
+      let letter = word[i];
+      if (!(letter in node.children)) {
+        node.children[letter] = new Node();
+      }
+      node = node.children[letter];
+    }
+    node.isTerminal = true;
+  }
 
-  //     if ()
-  //   }
-  // }
+  searchRecur(word, root = this.root) {
+    if (word.length === 0) {
+      if (root.isTerminal) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    let letter = word[0];
+    if (letter in root.children) {
+      return this.searchRecur(word.slice(1), root.children[letter]);
+    } else {
+      return false;
+    }
+  }
+
+  searchIter(word) {
+    let node = this.root;
+    for (let i = 0; i < word.length; i++) {
+      let letter = word[i];
+      if (!(letter in node.children)) {
+        return false;
+      }
+      node = node.children[letter];
+    }
+    return node.isTerminal;
+  }
+
+  wordsWithPrefix(prefix, root = this.root) {
+    if (prefix.length === 0) {
+      let allWords = [];
+      if (root.isTerminal) allWords.push('');
+
+      for (let letter in root.children) {
+        let child = root.children[letter];
+        let suffixes = this.wordsWithPrefix(prefix, child);
+        let words = suffixes.map(suffix => letter + suffix);
+        allWords.push(...words);
+      }
+      return allWords;
+    } else {
+      let firstLetter = prefix[0];
+      let child = root.children[firstLetter];
+      if (child === undefined) {
+        return [];
+      } else {
+        let suffixes = this.wordsWithPrefix(prefix.slice(1), child);
+        let words = suffixes.map(suffix => firstLetter + suffix);
+        return words;
+      }
+    }
+  }
+
 }
 
 module.exports = {
